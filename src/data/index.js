bcdc24aeece3f8872efe019c35a7e5c91935d8be
Giusseppe PC <giudelvecchio@gmail.com>
2020-07-26 16:34:51 -0400

    added all pics

--- src/data/index.js
+++ src/data/index.js
@@ -1,21 +1,47 @@
-import prof1 from '../images/headshot.jpeg'
 import bracket1314 from '../images/13-14/13-14bracket.jpg'
 import standings1314 from '../images/13-14/13-14standings.jpg'
 import winningTeamImg1314 from '../images/13-14/13-14winningteam.jpg'
+import tommy from '../images/profilePics/tommy.jpg'
+
 
 import bracket1415 from '../images/14-15/14-15bracket.jpg'
 import standings1415 from '../images/14-15/14-15standings.jpg'
 import winningTeamImg1415 from '../images/14-15/14-15winningteam.jpg'
+import steven from '../images/profilePics/steven.jpg'
+
 
 import bracket1516 from '../images/15-16/15-16bracket.jpg'
 import standings1516 from '../images/15-16/15-16standings.jpg'
 import winningTeamImg1516 from '../images/15-16/15-16winningteam.jpg'
+import whalen from '../images/profilePics/whalen.jpg'
+
+import bracket1617 from '../images/16-17/16-17bracket.jpg'
+import standings1617 from '../images/16-17/16-17standings.jpg'
+import winningTeamImg1617 from '../images/16-17/16-17winningteam.jpg'
+import iacono from '../images/profilePics/iacono.jpg'
+
+
+import bracket1718 from '../images/17-18/17-18bracket.jpg'
+import standings1718 from '../images/17-18/17-18standings.jpg'
+import winningTeamImg1718 from '../images/17-18/17-18winningteam.jpg'
+import joe from '../images/profilePics/joe.jpg'
+
+
+import bracket1819 from '../images/18-19/18-19bracket.jpg'
+import standings1819 from '../images/18-19/18-19standings.jpg'
+import winningTeamImg1819 from '../images/18-19/18-19winningteam.jpg'
+
+
+import bracket1920 from '../images/19-20/19-20bracket.jpg'
+import standings1920 from '../images/19-20/19-20standings.jpg'
+import winningTeamImg1920 from '../images/19-20/19-20winningteam.jpg'
+import josh from '../images/profilePics/josh.jpg'
 
 export const historyArr = [
     {
         year: '2019-20',
-        sliderPicsArr: [],
-        profilePic: prof1,
+        sliderPicsArr: [bracket1920,standings1920, winningTeamImg1920],
+        profilePic: josh,
         ownerName: 'Josh', 
         teamName: "Josh's Beast Team",
         teamMVP: "Michael Thomas",
@@ -23,8 +49,8 @@ export const historyArr = [
     },
     {
         year: '2018-19',
-        sliderPicsArr: [],
-        profilePic: prof1,
+        sliderPicsArr: [bracket1819,standings1819, winningTeamImg1819],
+        profilePic: whalen,
         ownerName: 'Whalen', 
         teamName: "Kramer says HeyBuddy",
         teamMVP: "Christian McCaffrey",
@@ -32,8 +58,8 @@ export const historyArr = [
     },
     {
         year: '2017-18',
-        sliderPicsArr: [],
-        profilePic: prof1,
+        sliderPicsArr: [bracket1718,standings1718, winningTeamImg1718],
+        profilePic: joe,
         ownerName: 'Joe', 
         teamName: "Daddy's Shower Buds",
         teamMVP: "Todd Gurley II",
@@ -41,8 +67,8 @@ export const historyArr = [
     },
     {
         year: '2016-17',
-        sliderPicsArr: [],
-        profilePic: prof1,
+        sliderPicsArr: [bracket1617,standings1617, winningTeamImg1617],
+        profilePic: iacono,
         ownerName: 'Iacono', 
         teamName: "Michael's Nice Team",
         teamMVP: "David Johnson",
@@ -51,7 +77,7 @@ export const historyArr = [
     {
         year: '2015-16',
         sliderPicsArr: [bracket1516, standings1516, winningTeamImg1516],
-        profilePic: prof1,
+        profilePic: whalen,
         ownerName: 'Whalen', 
         teamName: "Breaking Bad",
         teamMVP: "Carson Palmer",
@@ -60,7 +86,7 @@ export const historyArr = [
     {
         year: '2014-15',
         sliderPicsArr: [bracket1415, standings1415, winningTeamImg1415],
-        profilePic: prof1,
+        profilePic: steven,
         ownerName: 'Steven', 
         teamName: "Washington Sentinels",
         teamMVP: "Andrew Luck",
@@ -69,7 +95,7 @@ export const historyArr = [
     {
         year: '2013-14',
         sliderPicsArr: [bracket1314, standings1314, winningTeamImg1314],
-        profilePic: prof1,
+        profilePic: tommy,
         ownerName: 'Tommy', 
         teamName: "The Stugots",
         teamMVP: "Matt Forte",
